52f56fb10440f91212e62a9cc19da009156f3f41
diogo <dipm@novonordisk.com>
2023-05-19 14:51:10 +0200

    feat: add package src

---
+++ src/main.py
@@ -0,0 +1,15 @@
+"""
+API Reference
+=============
+
+This is the python package for the sphinx demo.
+
+.. toctree::
+    :maxdepth: 1
+
+    api/utils
+"""
+from utils import arithmetic
+
+if __name__ == '__main__':
+    print('The division of 1 by 2 is {}'.format(arithmetic.divide(1, 2)))
